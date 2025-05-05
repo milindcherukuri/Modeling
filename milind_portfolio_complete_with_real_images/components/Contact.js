6d0cf7268a02db805de098a9b16221ca348722ae
Cherukuri milind <42863247+milindcherukuri@users.noreply.github.com>
2025-05-05 00:00:56 -0500

    Update Contact.js

--- milind_portfolio_complete_with_real_images/components/Contact.js
+++ milind_portfolio_complete_with_real_images/components/Contact.js
@@ -5,6 +5,9 @@ export default function Contact() {
       <p className="text-lg mb-2">
         Email: <a href="mailto:cherukurimilind@gmail.com" className="underline hover:text-gray-300">cherukurimilind@gmail.com</a>
       </p>
+      <p className="text-lg mb-2">
+        Phone: <a className="underline hover:text-gray-300">281-781-3316</a>
+      </p>
       <p className="text-lg mb-4">
         Instagram: <a href="https://www.instagram.com/milind_cherukuri/" className="underline hover:text-gray-300">@milind_cherukuri</a>
       </p>
